143515a9b16ed7ad3964e6cc9fe72d91bf81fd83
Denis Fakhrtdinov <me@shizz.ru>
2013-10-26 05:04:50 +0400

    deadlock ix

--- apps/hydra/src/queue/hydra_queue.erl
+++ apps/hydra/src/queue/hydra_queue.erl
@@ -13,9 +13,9 @@
 -spec push(non_neg_integer(), term()) -> {ok, term()}.
 push(Priority, Payload)
 when is_integer(Priority), Priority < 6, Priority > 0 ->
-    gen_server:call(hydra_queue_tail, ?PUSH_CMD(Priority, Payload), infinity).
+    gen_server:call(hydra_queue_tail, ?PUSH_CMD(Priority, Payload), 60000).
 
 
 -spec pull(non_neg_integer()) -> {error, empty} | {ok, list()}.
 pull(Count) when is_integer(Count) ->
-    gen_server:call(hydra_queue_head, ?PULL_CMD(Count), infinity).
+    gen_server:call(hydra_queue_head, ?PULL_CMD(Count), 60000).
