0690755ee99176483d09f0f4eedeb3938ba4d99f
Denis Fakhrtdinov <me@shizz.ru>
2014-04-15 23:59:27 +0400

    tiny fix; fixes #2

--- apps/hydra/src/hydra_app.erl
+++ apps/hydra/src/hydra_app.erl
@@ -11,7 +11,7 @@
 
 
 start(_StartType, _StartArgs) ->
-    ok = unicorn:load(<<"/Users/shizz/code/_etc/squadder/hydra/config.json">>, fun loader/1, fun validator/1),
+    ok = unicorn:load(hydra_env:get(config), fun loader/1, fun validator/1),
     hydra_sup:start_link().
 
 stop(_State) ->
